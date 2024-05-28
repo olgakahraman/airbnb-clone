23aa875c574e2a6ae40308637847931011231fdd
olgakahraman <ol.khrmn@gmail.com>
2024-05-28 15:35:53 -0400

    created navbar and added logo image to it and some basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import Navbar from "./components/Navbar"
+import "./App.css";
 
 function App() {
-  
-
   return (
-   <div><h1>App is working</h1></div>
-  )
+    <div className="container">
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
