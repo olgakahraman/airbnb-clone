d7cdc86a3ec497a3b6ed2f2ae90d0a790f47524e
olgakahraman <ol.khrmn@gmail.com>
2024-05-29 20:44:55 -0400

    created a hero section and added some content and styling to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero(){
+    return (
+        <section className="hero">
+           <img src="../images/photo-grid.png" alt="grid of images" className="grid" />
+           <h1>Online Experiences</h1>
+           <p>Join interactive activities led by one-of-a-kind hosts - all without leaving home.</p>
+        </section>
+    )
+}
