57486eb228d0aeac0fb22aab560d8daefd9fc068
olgakahraman <ol.khrmn@gmail.com>
2024-06-07 19:35:35 -0400

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <img src="../images/airbnb-logo.png" alt="airbnb logo" className="airbnb-logo" />
+      <img
+        src="../images/airbnb-logo.png"
+        alt="airbnb logo"
+        className="airbnb-logo"
+      />
     </nav>
   );
 }
